1e00415ccf3e68272f31db7d62a54c9cdf12a512
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-05-02 16:11:09 -0600

    Added rest endpoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,16 +1,25 @@
 const routes = require('express').Router();
 const connect = require("../DB/connection");
 //const contactsController = require("../controllers/contacts");
+const ObjectId = require('mongodb').ObjectId;
 
 routes.get('/', (req, res) => {
-  connect
-    .getCollection()
-    .find()
-    .toArray((err, result) => {
-      if (err) throw err;
-      res.json(result);
-      console.log("Contacts Query Successful!");
-    });
+  const results = connect.getCollection().find();
+  results.toArray().then((documents) => {
+    res.status(200).json(documents);
+    console.log("Returned All Contacts")
+  }); 
+  
+});
+
+routes.get('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const results = connect.getCollection().find({_id: contactId});
+  results.toArray().then((documents) => {
+    res.status(200).json(documents[0]);
+    console.log(`Returned Contact ${req.params.id}`);
+  }); 
+
 });
 
 module.exports = routes;
