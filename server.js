b2c079ffe2cadfc3cb54b033e5ab6c80fa76f185
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2022-05-07 20:23:11 -0600

    Added useNewUrlParser

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const port = process.env.PORT || 8080;
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
